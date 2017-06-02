5aae0b664648706f88f69183dac8ddb15de589fe
YangZhou <21606948@qq.com>
2017-06-02 17:19:43 +0800

    建站，配好数据库

--- mysite/stock/views.py
+++ mysite/stock/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import tushare as ts
 # Create your views here.
+
+def home(request):
+	return HttpResponse("Hello World")
+
